e0e2c05022550dd671c6c116147a90f426295311
Samuel Nogueira <samuelnbacelar@hotmail.com>
2020-03-04 13:39:44 -0300

    estrutura de pastas básica e models adicionados

---
+++ back-end/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'A1234',
+    database: 'main',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
