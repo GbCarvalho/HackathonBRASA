78204a2bb7ae0e02467893b565c98eb4c08b0a14
Samuel Nogueira <samuelnbacelar@hotmail.com>
2020-03-08 16:20:10 -0300

    file and transaction models and migrations created

---
+++ back-end/src/app/models/Transaction.js
@@ -0,0 +1,23 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Transaction extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        description: Sequelize.STRING,
+        value: Sequelize.FLOAT,
+        payment_type: Sequelize.TINYINT,
+        transaction_type: Sequelize.TINYINT,
+      }
+    );
+    return this
+  }
+  static associate(models) {
+    this.belongsTo(models.Receipt, { foreignKey: 'receipt_id', as: 'receipt'});
+    this.belongsTo(models.Category, { foreignKey: 'category_id', as: 'category'});
+    this.belongsTo(models.User, {foreignKey: 'user_id', as: 'user'});
+  }
+
+}
+
+export default Transaction();
