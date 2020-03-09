c3df92e08afa46a4f716f627e95a732d727f6edb
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2020-03-09 18:32:26 -0300

    Initial changes for Receipt API

--- back-end/src/app/models/Receipt.js
+++ back-end/src/app/models/Receipt.js
@@ -1,33 +1,37 @@
-import Sequelize, { Model } from 'sequelize';
+import Sequelize, { Model } from "sequelize";
 
 class Receipt extends Model {
-	static init(sequelize){
-		super.init(
-			{
-				access_key: Sequelize.STRING,
-				reciept_date: Sequelize.DATE,
-				user_cpf: Sequelize.STRING,
-				reciept_value: Sequelize.FLOAT,
-				taxes_value: Sequelize.FLOAT,
-			},
-			{
-				sequelize,
-			},
-		);
+  static init(sequelize) {
+    super.init(
+      {
+        access_key: Sequelize.STRING,
+        receipt_date: Sequelize.DATE,
+        receipt_value: Sequelize.FLOAT,
+        taxes_value: Sequelize.FLOAT,
+        place: Sequelize.STRING,
+        provider: Sequelize.STRING,
+        provider_trade: Sequelize.STRING
+      },
+      {
+        sequelize
+      }
+    );
 
-		return this;
-	}
+    return this;
+  }
 
-	static associate(models) {
-		this.belongsToMany(models.Product, {
-			through: 'Product_Receipt',
-			as: 'products',
-			foreignKey: 'receipt_id',
-			otherKey: 'product_id'
-		});
-		this.belongsTo(models.Provider, { foreignKey: 'provider_id', as: 'provider' })
-	}
-
+  static associate(models) {
+    this.belongsToMany(models.Product, {
+      through: "Product_Receipt",
+      as: "products",
+      foreignKey: "receipt_id",
+      otherKey: "product_id"
+    });
+    this.belongsTo(models.Provider, {
+      foreignKey: "provider_id",
+      as: "provider"
+    });
+  }
 }
 
 export default Receipt;
