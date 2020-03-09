92690fee91e0e122e0b0a360723b3326af98d134
s7Thiago <thyagosousasilva@gmail.com>
2020-03-09 18:58:13 -0300

    Testando requisição a API da HG Brasil e exibindo os dados na tela

--- front-end/nu_gastos/lib/screens/home.dart
+++ front-end/nu_gastos/lib/screens/home.dart
@@ -57,7 +57,7 @@ class _HomeState extends State<Home> {
               Center(
                 child: Text(pages[page]),
               ),
-              RelatoriosWidgwt(),
+              RelatoriosWidget(),
             ],
           );
         },
