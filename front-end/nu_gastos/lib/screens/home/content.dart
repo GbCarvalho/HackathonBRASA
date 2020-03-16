db43dc9e972ab9f3ebe0a73d1c004e93d473f330
Thiago Sousa <thyagosousasilva@gmail.com>
2020-03-16 10:47:48 -0300

    Refatorando o código
    Deixando o código mais modularizado

--- front-end/nu_gastos/lib/screens/home/content.dart
+++ front-end/nu_gastos/lib/screens/home/content.dart
@@ -20,65 +20,66 @@ class HomeContentWidget extends StatelessWidget {
         itemCount: movimentacoes.length,
         itemBuilder: (BuildContext context, int index) {
           return Column(
-            children: <Widget>[
-              ListTile(
-                leading: Column(
-                  children: <Widget>[
-                    Spacer(),
-                    ClipOval(
-                      child: Container(
-                        color: main.nubankRoxoEscuro,
-                        child: Icon(
-                          movimentacoes[index].icon,
-                          color:
-                              getColorByTransactionType(movimentacoes[index]),
-                          size: 45,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                enabled: true,
-                title: Row(
-                  children: <Widget>[
-                    Text(
-                      '${movimentacoes[index].nome}',
-                      style: TextStyle(
-                          color:
-                              getColorByTransactionType(movimentacoes[index]),
-                          fontSize: 20),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Icon(
-                        Icons.edit,
-                        size: 15,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: Text(
-                  '${movimentacoes[index].data.split(' ')[0]}',
-                  style: TextStyle(color: main.nubankCinza),
-                ),
-                trailing: Text(
-                  'R\$ ${movimentacoes[index].valor}',
-                  style: TextStyle(
-                      color: getColorByTransactionType(movimentacoes[index]),
-                      fontSize: 25),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Divider(
-                  thickness: 2,
-                  color: Colors.black.withOpacity(0.15),
-                ),
-              )
-            ],
+            children: _buildListItem(index),
           );
         },
       ),
     );
   }
+
+  // * Responsável por contruir cada item da lista de transações
+  _buildListItem(int index) => [
+        ListTile(
+          leading: Column(
+            children: <Widget>[
+              Spacer(),
+              ClipOval(
+                child: Container(
+                  color: main.nubankRoxoEscuro,
+                  child: Icon(
+                    movimentacoes[index].icon,
+                    color: getColorByTransactionType(movimentacoes[index]),
+                    size: 45,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          enabled: true,
+          title: Row(
+            children: <Widget>[
+              Text(
+                '${movimentacoes[index].nome}',
+                style: TextStyle(
+                    color: getColorByTransactionType(movimentacoes[index]),
+                    fontSize: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Icon(
+                  Icons.edit,
+                  size: 15,
+                ),
+              ),
+            ],
+          ),
+          subtitle: Text(
+            '${movimentacoes[index].data.split(' ')[0]}',
+            style: TextStyle(color: main.nubankCinza),
+          ),
+          trailing: Text(
+            'R\$ ${movimentacoes[index].valor}',
+            style: TextStyle(
+                color: getColorByTransactionType(movimentacoes[index]),
+                fontSize: 25),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Divider(
+            thickness: 2,
+            color: Colors.black.withOpacity(0.15),
+          ),
+        )
+      ];
 }
