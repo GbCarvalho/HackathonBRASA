7b1d1238ba4ddbffe02d2ecef944f514e0d8e87a
Thiago Sousa <thyagosousasilva@gmail.com>
2020-03-16 15:40:28 -0300

    Melhorando a listagem de matérias

--- front-end/nu_gastos/lib/screens/educacao/educacao.dart
+++ front-end/nu_gastos/lib/screens/educacao/educacao.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nu_gasto/main.dart' as main;
 import 'package:nu_gasto/screens/educacao/materia_webviewer.dart';
+import 'package:nu_gasto/model/materia.dart';
 //import 'package:webview_flutter/webview_flutter.dart';
 
 var title = 'Educação';
@@ -8,6 +9,11 @@ var title = 'Educação';
 String get getTitle => title;
 
 class EducacaoWidget extends StatefulWidget {
+  // * Materias que aparecerão na lista
+  List<Materia> materias;
+
+  EducacaoWidget({Key key, this.materias = const []}) : super(key: key);
+
   @override
   _EducacaoWidgetState createState() => _EducacaoWidgetState();
 }
@@ -18,35 +24,79 @@ class _EducacaoWidgetState extends State<EducacaoWidget> {
     return Container(
         color: main.nubankRoxoEscuroClaro.withOpacity(0.86),
         child: ListView(
-          children: <Widget>[
-            Container(
-              color: main.nubankRoxoEscuro,
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MateriaWebviewer()));
-                    },
-                    leading: Image.network(
-                        'https://i2.wp.com/blog.nubank.com.br/wp-content/uploads/2020/01/ha%CC%81bitos-financeiros-para-guardar-dinheiro_header.jpg?w=1440&ssl=1'),
-                    title: Text(
-                      'Como criar bons hábitos financeiros?',
-                      style: TextStyle(decoration: TextDecoration.underline),
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Divider(
-                        thickness: 2,
-                        color: Colors.black.withOpacity(0.15),
-                      ))
-                ],
+          children: _buildCardList(),
+        ));
+  }
+
+  _buildCardList() {
+    var list = <Widget>[];
+
+    print('LISTA DE MATERIAS RECEBIDA: ${widget.materias}');
+
+    widget.materias.forEach((materia) {
+      list.add(Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: GestureDetector(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MateriaWebviewer(
+                destinationURL: materia.url,
               ),
             ),
-          ],
-        ));
+          ),
+          child: Column(
+            children: <Widget>[
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                color: main.nubankRoxoPrincipal,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.45,
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                          child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: main.nubankRoxoPrincipal,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                          ),
+                        ),
+                        child: Image.network(
+                          materia.imageURL,
+                          fit: BoxFit.cover,
+                        ),
+                      )),
+                      Expanded(
+                          child: Container(
+                        child: Center(
+                          child: Text(
+                            materia.title,
+                            style: TextStyle(
+                              fontSize: 45,
+                              // fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ))
+                    ],
+                  ),
+                ),
+              ),
+              Divider(
+                color: Colors.transparent,
+              )
+            ],
+          ),
+        ),
+      ));
+    });
+
+    return list;
   }
 }
